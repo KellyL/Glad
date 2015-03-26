5e0eb26fb69942d620e0d60140ccc3853db9062d
KellyL <kl365@hotmail.com>
2015-03-25 19:11:18 -0500

    Initial Commit+++ Customer Model and updated Gemlist

--- db/migrate/20150325235253_create_customers.rb
+++ db/migrate/20150325235253_create_customers.rb
@@ -1,9 +1,9 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-    	t.string name
-    	t.string order
-    	t.double orderTotal
+    	t.string :name
+    	t.string :order
+    	t.float :orderTotal
         t.timestamps null: false
     end
   end
